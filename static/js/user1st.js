c7f7c7cbb423d2be4b5dd9014662b385b3116764
Amy Groshek <amy@amygroshek.com>
2018-08-08 15:29:09 -0500

    Add index to front page, footer update

---
+++ static/js/user1st.js
@@ -0,0 +1,17 @@
+// console.log('user1st.js loaded');
+$( document ).ready(function() {
+    // console.log( "ready!" );
+
+    // Focus management for clicks on menu
+    // $('.topics > li.haschildren:not(.active) > div > i.category-icon, .topics > li.haschildren:not(.active) > div > a').click(function(e) {
+    //     console.log('click');
+    //     console.log($(e.target).parent('div').siblings('ul').find('li').first());
+    //     $(e.target).parent('div').siblings('ul').find('li').first().focus();
+    // });
+
+    // $('.topics > li.haschildren:not(.active) > div > a:focus')
+    //     // .is(':focus')
+    //     .click(function(e) {
+    //         console.log('click');
+    // });
+});
